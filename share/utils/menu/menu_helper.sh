6192f6c68647e8a20365ff8db763d17ca6689a20
Rem01Gaming <Rem01_Gaming@proton.me>
2024-03-22 14:59:09 +0700

    menu: Add fzf_select spesifically for multi-line option
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- share/utils/menu/menu_helper.sh
+++ share/utils/menu/menu_helper.sh
@@ -17,8 +17,13 @@
 # Copyright (C) 2023-2024 Rem01Gaming
 
 fzf_select() {
-	options=($(echo $1))
-	selected_option=$(echo -e "${options[@]}" | fzf $([[ $3 == "-n" ]] && echo "--select-1") --reverse --cycle --prompt "$2")
+options=($(echo $1))
+selected_option=$(printf "%s\n" "${options[@]}" | fzf --reverse --cycle --prompt "$2")
+echo $selected_option
+}
+
+fzf_select_n() {
+	selected_option=$(echo -e "$1" | fzf --select-1 --reverse --cycle --prompt "$2")
 	echo $selected_option
 }
 
