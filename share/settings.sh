a901e08d883d55108e10ec575f885b1ef345b650
Rem01Gaming <Rem01_Gaming@proton.me>
2024-06-19 22:53:58 +0700

    setting: Add an Easter Egg (miau)
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- share/settings.sh
+++ share/settings.sh
@@ -17,13 +17,62 @@
 # Copyright (C) 2023-2024 Rem01Gaming
 
 execstoredcmd_switch() {
+	# Easter Egg #1
+	# Kanged from Namida app, hehe
+	easteregg() {
+		dialog1() {
+			clear
+			tput civis
+			echo -e "\na- ano...\n\nthis one is actually supposed to be for supporters, if you don't mind you can support Origami Kernel Manager and get the power to unleash this cool feature\nor you just wanna use it like that? mattaku" | fold -s -w ${LINE}
+			local selected=$(fzy_select "Support\nLeave it disabled\nUmm..." "")
+			case $selected in
+			Support) donate && easteregg_output="TRUE" ;;
+			"Leave it disabled") easteregg_output="FALSE" ;;
+			"Umm...") dialog2 ;;
+			esac
+		}
+
+		dialog2() {
+			clear
+			tput civis
+			echo -e "\nEH? YOU DON'T WANT TO SUPPORT?" | fold -s -w ${LINE}
+			local selected=$(fzy_select "Yes\nNo" "")
+			case $selected in
+			Yes) donate && easteregg_output="TRUE" ;;
+			No) dialog3 ;;
+			esac
+		}
+
+		dialog3() {
+			clear
+			tput civis
+			echo -e "\nhidoii ಥ⁠‿⁠ಥ here use it as u can,\ndw im not upset or anything ^^, or am i?" | fold -s -w ${LINE}
+			local selected=$(fzy_select "UNLOCK\nSUPPORT" "")
+			case $selected in
+			SUPPORT) donate && easteregg_output="TRUE" ;;
+			UNLOCK) easteregg_output="TRUE" ;;
+			esac
+		}
+
+		dialog1
+	}
+
 	case $(fzf_select "Enable Disable" "Apply previous settings from last session:  ") in
-	Enable) execstoredcmd_db TRUE ;;
+	Enable) easteregg && execstoredcmd_db $easteregg_output ;;
 	Disable) execstoredcmd_db FALSE ;;
 	esac
+
+	unset easteregg_output
 }
 
 execstoredcmd_risky_switch() {
+	if [ $(sql_query "SELECT execstoredcmd FROM tb_info;") -eq 0 ]; then
+		echo -e "\n[-] Enable Apply previous settings feature first"
+		echo "[*] Hit enter to back to main menu"
+		read -r -s
+		return 1
+	fi
+
 	case $(fzf_select "Enable Disable" "Allow risky execution:  ") in
 	Enable) execstoredcmd_risky_db TRUE ;;
 	Disable) execstoredcmd_risky_db FALSE ;;
