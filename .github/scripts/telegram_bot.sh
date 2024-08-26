cae29e478b657716ad0a94d2d0d063e0000f392b
fukiame <fukiame@proton.me>
2024-08-26 22:45:57 +0700

    workflow: fix telegram bot
    - move back to markdownv2
    - escape all special characters
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- .github/scripts/telegram_bot.sh
+++ .github/scripts/telegram_bot.sh
@@ -1,15 +1,18 @@
 #!/bin/env bash
 
-msg="<b>$title</b><br>
-#ci_$version<br><br>
-<blockquote>$commit_message</blockquote><br>
-<a href="$COMMIT_URL">Commit</a><br>
-<a href="$RUN_URL">Workflow run</a>"
+msg="**$TITLE**
+\\#ci\\_$VERSION
+\`\`\`
+$COMMIT_MESSAGE
+\`\`\`
+[Commit]($COMMIT_URL)
+[Workflow run]($RUN_URL)
+"
 
 file="$1"
 
 curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
 	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
+	-F "parse_mode=markdownv2" \
 	-F caption="$msg"
