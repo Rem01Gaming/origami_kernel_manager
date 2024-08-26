583600ec4e8aea6963526934d9696d536bf84131
Rem01Gaming <Rem01_Gaming@proton.me>
2024-08-26 21:24:46 +0700

    workflow: Try to fix 400 telegram bot error {"ok":false,"error_code":400,"description":"Bad Request: can't parse entities: Can't find end of the entity starting at byte offset 9"}
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- .github/scripts/telegram_bot.sh
+++ .github/scripts/telegram_bot.sh
@@ -1,18 +1,15 @@
 #!/bin/env bash
 
-msg="**$title**
-#ci_$version
-
->> $commit_message
-
-[Commit]($commit_url)
-[Workflow run]($run_url)
-"
+msg="<b>$title</b><br>
+#ci_$version<br><br>
+<blockquote>$commit_message</blockquote><br>
+<a href="$COMMIT_URL">Commit</a><br>
+<a href="$RUN_URL">Workflow run</a>"
 
 file="$1"
 
 curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
 	-F "disable_web_page_preview=true" \
-	-F "parse_mode=markdown" \
+	-F "parse_mode=html" \
 	-F caption="$msg"
