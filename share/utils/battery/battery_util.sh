36c6c4fbb264cc891b3aaeb632d79e13ee9cafa6
Rem01Gaming <Rem01_Gaming@proton.me>
2025-01-03 19:51:25 +0700

    battery: Add Transision's bypass charging path
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- share/utils/battery/battery_util.sh
+++ share/utils/battery/battery_util.sh
@@ -53,6 +53,7 @@ test_chg_switches() {
 		"${battery_node_path}/battery_charging_enabled 1 0"
 		"/sys/class/hw_power/charger/charge_data/enable_charger 1 0"
 		"/sys/class/qcom-battery/input_suspend 0 1"
+		"/sys/devices/platform/charger/tran_aichg_disable_charger 0 1"
 		"/sys/devices/platform/huawei_charger/enable_charger 1 0"
 		"/sys/devices/platform/lge-unified-nodes/charging_completed 0 1"
 		"/sys/devices/platform/lge-unified-nodes/charging_enable 1 0"
